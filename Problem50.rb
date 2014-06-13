d6e5bd8112550dad1c3107604b1a69a3e64e1b64
Alan Bishop <djab1990@gmail.com>
2014-06-13 14:41:16 -0700

    Problem 50 in progress (lots of work left to do!)

--- Problem50.rb
+++ Problem50.rb
@@ -1,6 +1,6 @@
 =begin
 Project Euler Problem 50
-Written By Alan Bishop 6/11/14
+Written By Alan Bishop 6/13/14
 
 
 "The prime 41, can be written as the sum of six consecutive primes:
@@ -13,37 +13,44 @@ The longest sum of consecutive primes below one-thousand that adds to a prime, c
 Which prime, below one-million, can be written as the sum of the most consecutive primes?"
 =end
 
+
 start = Time.now
 
 def getAllPrimesTo(n)
-    primes = Array.new
-    for i in 2..n 
-        prime = true
-        for j in 2..Math.sqrt(i)
-            if(i%j == 0) 
-                prime = false
-                break
-            end
-        end
-        push(i) if prime
+    nums = [nil, nil, *2..n]
+    (2..Math.sqrt(n)).each do |i|
+        (i**2..n).step(i){|m| nums[m] = nil}  if nums[i]
     end
-    return primes
+    nums.compact
 end
 
 primes = getAllPrimesTo(1000000)
+puts "Found #{primes.length} Primes, Highest: #{primes.max}"
 
+maxPrime = 0
+sum = 0
+endN = 0
 
-longestPrimeSum = 0
-mostConsecPrimes = 0
-for i in 1..primes.length
-    sum = 0
-    consecPrimes = 0
-    for j in 0..primes.length
-       
+for startN in 0..primes.length
+    #Find highest valid num
+    for i in endN..primes.length
+        sum += primes[i]
+        endN = i
+        break if sum > primes.max
+        if primes.include?(sum) && maxPrime < sum
+            puts "New maxPrime = #{sum} (start = #{startN}, end = #{endN})"
+            maxPrime = sum
+        end
     end
+    #Get a good starting point for next iteration
+    while sum > primes.max || (!primes.include?(sum) && sum > 0)
+        sum -= primes[endN]
+        endN -= 1
+    end
+    sum -= primes[startN]
 end
 
 
-puts "Sum below 1,000,000 with longest consecutive primes sum is #{longestPrimeSum}"
+puts "Sum below 1,000,000 with longest consecutive primes sum is #{maxPrime}"
 
 puts "Total Runtime: #{Time.now-start} Seconds."
