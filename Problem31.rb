16d4d3a831b333d563981edcd84623599e755413
Alan Bishop <djab1990@gmail.com>
2014-08-19 10:35:53 -0700

    Problem 31 in Ruby complete

--- Problem31.rb
+++ Problem31.rb
@@ -13,18 +13,22 @@ It is possible to make £2 in the following way:
 How many different ways can £2 be made using any number of coins?"
 =end
 
-#Perhaps we should use a hash for this?
-
 start = Time.now
 
-
-
 $DESIRED_AMOUNT = 200
-amounts = [1,2,5,10,50,100,200]
-combos = 0
+coins = [1,2,5,10,20,50,100,200]
+combos = Array.new($DESIRED_AMOUNT + 1)
+combos[0] = 1;
 
+for coin in coins
+    for amt in coin..$DESIRED_AMOUNT
+        if(combos[amt] == nil)
+            combos[amt] = 0
+        end
+        combos[amt] += combos[amt - coin] 
+    end
+end
 
-
-puts "The number of possible combinations is #{combos}"
+puts "The number of possible combinations is #{combos[$DESIRED_AMOUNT]}"
 
 puts "Total Runtime: #{Time.now-start} Seconds."
